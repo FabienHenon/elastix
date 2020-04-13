e86049e2d9ee9f38e771e343b305acc8a72b9a5b
Fabien HENON <henon.fabien@softcreations.fr>
2020-04-13 18:42:10 +0200

    Add update query

--- lib/elastix/document.ex
+++ lib/elastix/document.ex
@@ -10,8 +10,20 @@ defmodule Elastix.Document do
 
   @doc false
   def index(elastic_url, index_name, type_name, id, data, query_params) do
-    elastic_url <> make_path(index_name, type_name, id, query_params)
+    (elastic_url <> make_path(index_name, type_name, id, query_params))
     |> HTTP.put(Poison.encode!(data))
+    |> process_response
+  end
+
+  @doc false
+  def update(elastic_url, index_name, type_name, id, data) do
+    index(elastic_url, index_name, type_name, id, data, [])
+  end
+
+  @doc false
+  def update(elastic_url, index_name, type_name, id, data, query_params) do
+    (elastic_url <> make_path_update(index_name, type_name, id, query_params))
+    |> HTTP.post(Poison.encode!(data))
     |> process_response
   end
 
@@ -22,7 +34,7 @@ defmodule Elastix.Document do
 
   @doc false
   def index_new(elastic_url, index_name, type_name, data, query_params) do
-    elastic_url <> make_path(index_name, type_name, query_params)
+    (elastic_url <> make_path(index_name, type_name, query_params))
     |> HTTP.post(Poison.encode!(data))
     |> process_response
   end
@@ -41,28 +53,38 @@ defmodule Elastix.Document do
 
   @doc false
   def get(elastic_url, index_name, type_name, id, query_params) do
-    elastic_url <> make_path(index_name, type_name, id, query_params)
-    |> HTTP.get
+    (elastic_url <> make_path(index_name, type_name, id, query_params))
+    |> HTTP.get()
     |> process_response
   end
 
   @doc false
   def delete(elastic_url, index_name, type_name, id) do
-    elastic_url <> make_path(index_name, type_name, id, [])
-    |> HTTP.delete
+    (elastic_url <> make_path(index_name, type_name, id, []))
+    |> HTTP.delete()
     |> process_response
   end
 
   @doc false
   def delete(elastic_url, index_name, type_name, id, query_params) do
-    elastic_url <> make_path(index_name, type_name, id, query_params)
-    |> HTTP.delete
+    (elastic_url <> make_path(index_name, type_name, id, query_params))
+    |> HTTP.delete()
     |> process_response
   end
 
   @doc false
   def make_path(index_name, type_name, id \\ nil, query_params) do
     path = "/#{index_name}/#{type_name}/#{id}"
+
+    case query_params do
+      [] -> path
+      _ -> add_query_params(path, query_params)
+    end
+  end
+
+  @doc false
+  def make_path_update(index_name, type_name, id \\ nil, query_params) do
+    path = "/#{index_name}/#{type_name}/#{id}/_update"
 
     case query_params do
       [] -> path
@@ -82,9 +104,10 @@ defmodule Elastix.Document do
 
   @doc false
   defp add_query_params(path, query_params) do
-    query_string = Enum.map_join query_params, "&", fn(param) ->
-      "#{elem(param, 0)}=#{elem(param, 1)}"
-    end
+    query_string =
+      Enum.map_join(query_params, "&", fn param ->
+        "#{elem(param, 0)}=#{elem(param, 1)}"
+      end)
 
     "#{path}?#{query_string}"
   end
